e73038e9680b73e780d977585a02fc99e8555a7b
samuel_natividade <samuell.natividade@gmail.com>
2025-02-04 21:03:20 -0300

    feat: implementar contrato

---
+++ src/contrato.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr, PositiveFloat, PositiveInt
+from datetime import datetime
+from enum import Enum
+
+class CategoriaEnum(str, Enum):
+        categoria1 = 'categoria1'
+        categoria2 = 'categoria2'
+        categoria3 = 'categoria3'
+
+class Vendas(BaseModel):
+        email: EmailStr
+        data: datetime
+        valor: PositiveFloat
+        produto: str
+        quantidade: PositiveInt
+        categoria: CategoriaEnum
+    
+ 
